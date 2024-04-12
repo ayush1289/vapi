b2848fc301cc7fc1040b19503c8f706659297625
Ayush Shankarpurkar <ayushshankarpurkar11@gmail.com>
2024-04-12 20:26:58 +0530

    ws + audio_file

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from fastapi.websockets import WebSocketDisconnect,WebSocketState
 import logging
 import uuid
 
-
+api_key='6010d092-894c-45bb-96c2-448f599a6c72'
+assistant_id='787065c5-8057-4fbe-b869-950ed4639a3c'
 
 
 app = FastAPI()
